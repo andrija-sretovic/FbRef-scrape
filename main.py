1c5fea34f2ce8038726d3fd95a107374aac39b85
andrija-sretovic <147065409+andrija-sretovic@users.noreply.github.com>
2024-01-06 03:01:51 +0100

    Add code for stats against, add today date

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+from datetime import datetime
+
+today = datetime.today().strftime('%d-%m-%Y')
 
 fbref_url_pl = 'https://fbref.com/en/comps/9/Premier-League-Stats'
 
@@ -14,7 +17,15 @@ table_names = ['stats_squads_standard_for', 'stats_squads_keeper_for', 'stats_sq
                'stats_squads_shooting_for', 'stats_squads_passing_for', 'stats_squads_passing_types_for',
                'stats_squads_gca_for', 'stats_squads_defense_for', 'stats_squads_possession_for']
 
+table_names_agg = ['stats_squads_standard_against', 'stats_squads_keeper_against', 'stats_squads_keeper_adv_against',
+                   'stats_squads_shooting_against', 'stats_squads_passing_against', 'stats_squads_passing_types_against',
+                   'stats_squads_gca_against', 'stats_squads_defense_against', 'stats_squads_possession_against']
+
+
 team_for_df = pd.DataFrame()
+team_agg_df = pd.DataFrame()
+
+# Get data for teams when attacking
 
 for name in table_names:
     stats_table = soup.find('table', id=name)
@@ -27,16 +38,34 @@ for name in table_names:
     stats_columns = stats[1]
     stats_df = pd.DataFrame(stats[2:], columns=stats_columns)
 
-    print(f'1: {stats_df}')
     if team_for_df.empty:
         team_for_df = stats_df
-        print(f'2: {team_for_df}')
     else:
         stats_df.drop(['# Pl', '90s'], axis=1, inplace=True)
-        print(f'3: {stats_df}')
-        team_for_df = pd.merge(team_for_df, stats_df, on='Squad', suffixes=(name, name))
-        print(f'4: {team_for_df}')
+        team_for_df = pd.merge(team_for_df, stats_df, on='Squad', how='left',
+                               suffixes=(f'-{name.split("_")[-2]}', f'-{name.split("_")[-2]}'))
 
+team_for_df.to_csv(f'PL team stats-{today}.csv', index=False)
 
-print(team_for_df)
+# Get data for teams when defending
+
+for name in table_names_agg:
+    stats_table = soup.find('table', id=name)
+    stats_agg = []
+
+    for row in stats_table.find_all('tr'):
+        team_data = [cell.text.strip() for cell in row.find_all(['th', 'td'])]
+        stats_agg.append(team_data)
+
+    stats_agg_columns = stats_agg[1]
+    stats_agg_df = pd.DataFrame(stats_agg[2:], columns=stats_agg_columns)
+
+    if team_agg_df.empty:
+        team_agg_df = stats_agg_df
+    else:
+        stats_agg_df.drop(['# Pl', '90s'], axis=1, inplace=True)
+        team_agg_df = pd.merge(team_agg_df, stats_agg_df, on='Squad', how='left',
+                               suffixes=(f'-{name.split("_")[-2]}', f'-{name.split("_")[-2]}'))
+
+team_agg_df.to_csv(f'PL team stats agg-{today}.csv', index=False)
 
